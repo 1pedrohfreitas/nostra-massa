57d975790279e62140a2a9586cbfddc462c40559
Pedro Henrique Freitas <1pedrohfreitas@gmail.com>
2024-05-09 22:40:38 -0300

    Subindo alteração

---
+++ script.sql
@@ -0,0 +1,7 @@
+ALTER TABLE public.cliente ADD taxaentrega float8 NULL;
+ALTER TABLE public.cliente ADD apartamento varchar(20) NULL;
+ALTER TABLE public.cliente ADD bloco varchar(20) NULL;
+ALTER TABLE public.cliente ADD numero varchar(30) NULL;
+ALTER TABLE public.cliente ADD bairro varchar(200) NULL;
+ALTER TABLE public.cliente ADD rua varchar(200) NULL;
+ALTER TABLE public.cliente ADD complemento varchar(300) NULL;
